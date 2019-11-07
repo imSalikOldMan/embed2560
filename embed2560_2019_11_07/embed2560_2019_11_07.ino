7a1868768ba79242ea43492034548c211134916d
imsalik1972 <imsalik1972@outlook.com>
2019-11-07 10:50:59 +0500

    Добавьте файлы проекта.

---
+++ embed2560_2019_11_07/embed2560_2019_11_07.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		embed2560_2019_11_07.ino
+ Created:	07.11.2019 10:50:47
+ Author:	salik
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
